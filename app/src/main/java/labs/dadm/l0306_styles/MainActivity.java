dc0790a3585376275bb1f5f7e49a0e996932169f
ddandres <ddandres@disca.upv.es>
2021-12-21 16:00:52 +0100

    Updated to API 31

--- app/src/main/java/labs/dadm/l0306_styles/MainActivity.java
+++ app/src/main/java/labs/dadm/l0306_styles/MainActivity.java
@@ -4,9 +4,9 @@
 
 package labs.dadm.l0306_styles;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
